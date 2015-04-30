a2c53461f8ae05bb120a45b5b109cdef85c6a25a
Robert <rkty13@gmail.com>
2015-04-30 19:26:09 -0400

    Made edits and removed secret.py from git history

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from flask import Flask, render_template, request, redirect, session
 import jinja2
 from pymongo import *
 import os
-from secret import *
 from utils import *
 import ast
 
 app = Flask(__name__)
 
-app.secret_key = APP_SECRET
-client = MongoClient(MONGO_CRED)
+app.secret_key = os.environ["APP_SECRET"]
+client = MongoClient(os.environ["MONGO_CRED"])
 db = client.get_default_database()
 users = db.users
 tutor_sessions = db.tutor_sessions
@@ -254,4 +253,7 @@ def sheet():
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
-	app.run(host='0.0.0.0', port=port,debug=True)
+	debug = True
+	if 'DYNO' in os.environ:
+	    debug = False
+	app.run(host='0.0.0.0', port=port, debug=debug)
